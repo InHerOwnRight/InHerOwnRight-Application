48b16754a9c6b6c41cc19fa77b41cfe8ff06f776
William Bolton <william.ellet@gmail.com>
2017-03-28 16:32:05 -0400

    141343023 Migration and schema dc_coverage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328202719) do
+ActiveRecord::Schema.define(version: 20170328203059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20170328202719) do
     t.string   "contributor"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "dc_coverages", force: :cascade do |t|
+    t.integer  "record_id"
+    t.string   "coverage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "dc_creators", force: :cascade do |t|
