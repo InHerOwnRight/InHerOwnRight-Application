7da07d23f0809708e20bf518e58a552a218f024d
William Bolton <william.ellet@gmail.com>
2017-03-28 15:59:09 -0400

    141343023 Migration and schema dc_languages

---
+++ db/migrate/20170328195758_create_dc_languages.rb
@@ -0,0 +1,9 @@
+class CreateDcLanguages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dc_languages do |t|
+      t.integer :record_id
+      t.string :language
+      t.timestamps
+    end
+  end
+end
