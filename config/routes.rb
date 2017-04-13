927368f0f36e4635d4ab8cd21d60b664722b019f
William Bolton <william.ellet@gmail.com>
2017-04-13 16:47:04 -0400

    143411299 Route for records/show

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :records, only: [:show]
+
   mount Blacklight::Engine => '/'
   root to: "catalog#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
