c2e18a6d6e5b4d4e17ced4c467c6bdc432c7e7e2
William Bolton <william.ellet@gmail.com>
2017-05-12 15:28:30 -0400

    143411299 Improve date in record’s view

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,12 @@
 class RecordsController < ApplicationController
 
-def show
-  @record = Record.find(params[:id])
-end
+  def show
+    @record = Record.find(params[:id])
+  end
+
+  def display_date_range(record)
+    "#{record.dc_dates.minimum(:date).year} &ndash; #{record.dc_dates.maximum(:date).year}".html_safe
+  end
+  helper_method :display_date_range
 
 end
