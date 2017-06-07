d2e57b419bf63bcf186bd297ea8ff80ea0771e81
William Bolton <william.ellet@gmail.com>
2017-06-07 16:37:36 -0400

    145849579 Link to repository from main image

--- app/models/dc_identifier.rb
+++ app/models/dc_identifier.rb
@@ -1,3 +1,8 @@
 class DcIdentifier < ActiveRecord::Base
   belongs_to :record
+
+  def identifier_is_url?
+    identifier.split(":").first == "http" || identifier.split(":").first == "https"
+  end
+
 end
