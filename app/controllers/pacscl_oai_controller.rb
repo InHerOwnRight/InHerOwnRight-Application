0baa14912c9b11d95f69cb7aa45c923a5a57ba78
William Bolton <william.ellet@gmail.com>
2017-06-28 10:30:47 -0400

    OAI controller

---
+++ app/controllers/pacscl_oai_controller.rb
@@ -0,0 +1,9 @@
+class PacsclOaiController < ApplicationController
+  def index
+    # Remove controller and action from the options. Rails adds them automatically.
+    options = params.delete_if { |k,v| %w{controller action}.include?(k) }
+    provider = PacsclOaiProvider.new
+    response = provider.process_request(options)
+    render text: response, content_type: "text/xml", layout: false
+  end
+end
