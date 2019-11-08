f991221df29415fe94fa3dfe867c1c10dff9fb4d
Haley Mesander <haleymesander@gmail.com>
2019-11-08 07:30:26 -0700

    Add friendly id for url slugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190128020156) do
+ActiveRecord::Schema.define(version: 20191108094657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,18 @@ ActiveRecord::Schema.define(version: 20190128020156) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+  end
+
   create_table "full_texts", force: :cascade do |t|
     t.integer  "record_id"
     t.text     "transcription"
@@ -198,6 +210,8 @@ ActiveRecord::Schema.define(version: 20190128020156) do
     t.string   "file_name"
     t.string   "thumbnail"
     t.integer  "collection_id"
+    t.string   "slug"
+    t.index ["slug"], name: "index_records_on_slug", unique: true, using: :btree
   end
 
   create_table "repositories", force: :cascade do |t|
