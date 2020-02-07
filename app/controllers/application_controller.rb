448ac7e614837f7c71658bccff0e69c4c46e6dea
Jordan Owens <jkowens@gmail.com>
2020-02-06 23:04:11 -0500

    Add Sentry monitoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
   helper_method :titleize_dropdown_links
 
   protect_from_forgery with: :exception
+
+  before_action :set_raven_context
+
+  def set_raven_context
+    # Include current_user_id and params with exceptions sent to Sentry
+    Raven.user_context(user_id: session[:current_user_id])
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
