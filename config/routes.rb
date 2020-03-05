cb3dc4465d26fb9b8692df8f8a13c0e61cc22b38
JP Lynch <JPLynch35@gmail.com>
2020-03-05 08:19:26 -0500

    Temporarily disable process image routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
 
   resources :oai, only: [:index], controller: 'pacscl_oai'
 
-  get '/process_images', to: 'process_images#index'
-  post '/process_images', to: 'process_images#create'
+  # get '/process_images', to: 'process_images#index'
+  # post '/process_images', to: 'process_images#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
