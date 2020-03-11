e85cc430dc15698e74a7bad372cc5f84d706bd6e
Jordan Owens <jkowens@gmail.com>
2020-03-11 00:40:22 -0400

    Update label for missing date facet

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+BlacklightRangeLimit.labels = {
+  missing: I18n.t('blacklight.range_limit.missing')
+}
