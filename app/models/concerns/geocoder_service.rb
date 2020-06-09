bcf19524e699ecf196ecfee0063397528fd3920a
JP Lynch <JPLynch35@gmail.com>
2020-06-09 10:50:39 -0400

    [#170705735] Escape invalid characters for geocode

--- app/models/concerns/geocoder_service.rb
+++ app/models/concerns/geocoder_service.rb
@@ -1,10 +1,13 @@
 class GeocoderService
   def self.call_open_street_maps(placename)
     sanitized_placename = SpacialSanitizer.execute(placename)
-    request = "https://nominatim.openstreetmap.org/search?format=json&q=#{sanitized_placename}"
-    response = HTTParty.get(request)
+    begin
+      request = "https://nominatim.openstreetmap.org/search?format=json&q=#{sanitized_placename}"
+      response = HTTParty.get(request)
+    rescue URI::InvalidURIError
+      escaped_request = "https://nominatim.openstreetmap.org/search?format=json&q=#{URI.escape(sanitized_placename)}"
+      response = HTTParty.get(escaped_request)
+    end
     OsmApiCall.create(placename: placename, sanitized_placename: sanitized_placename, request: request, response: response)
-  rescue => e
-    raise "Open Maps API call failed: #{e}"
   end
 end
