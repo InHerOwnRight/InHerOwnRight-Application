03f8969946b985b5c3d070fc2c234aced19ce006
JP Lynch <JPLynch35@gmail.com>
2020-06-30 14:38:53 -0400

    [#170704139] Fix pacscl collection index callbacks

--- app/models/pacscl_collection.rb
+++ app/models/pacscl_collection.rb
@@ -2,28 +2,24 @@ class PacsclCollection < ActiveRecord::Base
   belongs_to :repository
   has_many :dc_terms_is_part_ofs
   has_many :records, through: :dc_terms_is_part_ofs
-  after_create :update_associations_and_reindex_after_create
-  after_save :update_associations_and_reindex, if: :saved_change_to_detailed_name
+  after_save :update_associations_and_reindex, if: :saved_change_to_detailed_name?
   around_destroy :update_associations_and_reindex_after_destroy
 
   private
 
-  def update_associations_and_reindex_after_create
-    dc_terms_is_part_of_collection = DcTermsIsPartOf.where(is_part_of: detailed_name)
-    dc_terms_is_part_of_collection.each do |dc_terms_is_part_of|
-      dc_terms_is_part_of.pacscl_collection_id = id
-      dc_terms_is_part_of.save
-    end
-    Sunspot.index!(records)
-  end
-
   def update_associations_and_reindex
-    dc_terms_is_part_of_collection = dc_terms_is_part_ofs
+    dc_terms_is_part_of_collection = dc_terms_is_part_ofs + DcTermsIsPartOf.where(is_part_of: detailed_name)
+    dc_terms_is_part_of_collection.uniq!
+    record_collection = dc_terms_is_part_of_collection.map{ |dtipo| dtipo.record }
     dc_terms_is_part_of_collection.each do |dc_terms_is_part_of|
-      dc_terms_is_part_of.pacscl_collection_id = nil
+      if dc_terms_is_part_of.is_part_of == detailed_name
+        dc_terms_is_part_of.pacscl_collection_id = id
+      else
+        dc_terms_is_part_of.pacscl_collection_id = nil
+      end
       dc_terms_is_part_of.save
     end
-    Sunspot.index!(records)
+    Sunspot.index!(record_collection)
   end
 
   def update_associations_and_reindex_after_destroy
