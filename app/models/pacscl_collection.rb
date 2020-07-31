67d9fc5ec39b33a9e53d8b606f4c90082001a0a3
JP Lynch <JPLynch35@gmail.com>
2020-07-31 10:38:02 -0400

    [#170704139] Add dc relations to collections

--- app/models/pacscl_collection.rb
+++ app/models/pacscl_collection.rb
@@ -1,16 +1,33 @@
 class PacsclCollection < ActiveRecord::Base
   belongs_to :repository
   has_many :dc_terms_is_part_ofs
-  has_many :records, through: :dc_terms_is_part_ofs
+  has_many :dc_relations
   after_save :update_associations_and_reindex, if: :saved_change_to_detailed_name?
   around_destroy :update_associations_and_reindex_after_destroy
+
+  def associated_records
+    dc_terms_is_part_of_records = dc_terms_is_part_ofs.map { |dtipo| dtipo.record }
+    dc_relation_records = dc_relations.map { |dr| dr.record }
+
+    combined_records = dc_terms_is_part_of_records + dc_relation_records
+    combined_records.compact.uniq
+  end
 
   private
 
   def update_associations_and_reindex
     dc_terms_is_part_of_collection = dc_terms_is_part_ofs + DcTermsIsPartOf.where(is_part_of: detailed_name)
     dc_terms_is_part_of_collection.uniq!
-    record_collection = dc_terms_is_part_of_collection.map{ |dtipo| dtipo.record }
+
+    dc_relations_collection = dc_relations + DcRelation.where(relation: detailed_name)
+    dc_relations_collection.uniq!
+
+    dtipo_record_collection = dc_terms_is_part_of_collection.map{ |dtipo| dtipo.record }
+    dr_record_collection = dc_relations_collection.map{ |drc| drc.record }
+
+    combined_record_collection = dtipo_record_collection + dr_record_collection
+    combined_record_collection.uniq!
+
     dc_terms_is_part_of_collection.each do |dc_terms_is_part_of|
       if dc_terms_is_part_of.is_part_of == detailed_name
         dc_terms_is_part_of.pacscl_collection_id = id
@@ -19,16 +36,34 @@ class PacsclCollection < ActiveRecord::Base
       end
       dc_terms_is_part_of.save
     end
-    Sunspot.index!(record_collection)
+
+    dc_relations_collection.each do |dc_relation|
+      if dc_relation.relation == detailed_name
+        dc_relation.pacscl_collection_id = id
+      else
+        dc_relation.pacscl_collection_id = nil
+      end
+      dc_relation.save
+    end
+
+    Sunspot.index!(combined_record_collection)
   end
 
   def update_associations_and_reindex_after_destroy
+    record_collection = associated_records
+
     dc_terms_is_part_ofs.each do |dc_terms_is_part_of|
       dc_terms_is_part_of.pacscl_collection_id = nil
       dc_terms_is_part_of.save
     end
-    record_collection = records
+
+    dc_relations.each do |dc_relation|
+      dc_relation.pacscl_collection_id = nil
+      dc_relation.save
+    end
+
     yield
+
     Sunspot.index!(record_collection)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200708204108) do
+ActiveRecord::Schema.define(version: 20200731135906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20200708204108) do
     t.string "relation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pacscl_collection_id"
   end
 
   create_table "dc_research_interests", force: :cascade do |t|
