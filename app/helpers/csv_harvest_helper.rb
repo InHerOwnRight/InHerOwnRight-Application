7368aaa3ac3230d34ac9a7a4cf3971393588044e
Haley Mesander <haleymesander@gmail.com>
2020-09-20 17:11:25 -0600

    Update import_images process for csv harvest

--- app/helpers/csv_harvest_helper.rb
+++ app/helpers/csv_harvest_helper.rb
@@ -1,4 +1,5 @@
 require "csv"
+require 'aws-sdk-s3'
 
 module CsvHarvestHelper
   def self.initiate(harvest)
@@ -18,7 +19,7 @@ module CsvHarvestHelper
     end
     self.delay(:queue => "csv_#{@repository.short_name.downcase.gsub(" ","_")}").create_raw_records(harvest) if harvest.error.nil?
     self.delay(:queue => "csv_#{@repository.short_name.downcase.gsub(" ","_")}").create_records(harvest) if harvest.error.nil?
-    self.import_images(harvest) if harvest.error.nil?
+    self.delay(:queue => "csv_#{@repository.short_name.downcase.gsub(" ","_")}").import_images(harvest) if harvest.error.nil?
     self.delay(:queue => "csv_#{@repository.short_name.downcase.gsub(" ","_")}").index_records(harvest) if harvest.error.nil?
   end
 
@@ -145,8 +146,12 @@ module CsvHarvestHelper
 
   def self.import_images(harvest)
     begin
-      Delayed::Job.enqueue(DelayedRake.new("import_images:#{@repository.image_task}['#{harvest}']"), queue: "csv_#{@repository.short_name.downcase.gsub(" ","_")}")
-      Delayed::Job.enqueue(DelayedRake.new("import_images:clean_up_collection_imgs"), queue: "csv_#{@repository.short_name.downcase.gsub(" ","_")}")
+      harvest.update(status: 2)
+      region = 'us-east-2'
+      s3 = Aws::S3::Resource.new(region: region, access_key_id: ENV["AWS_ACCESS_KEY_ID"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"])
+      bucket = s3.bucket('pacscl-production')
+      import_repo_images(harvest, region, s3, bucket)
+      clean_up_collection_imgs
     rescue => e
       harvest.update(status: 5, error: e.message )
     end
@@ -159,6 +164,378 @@ module CsvHarvestHelper
       harvest.update(status: 4)
     rescue => e
       harvest.update(status: 5, error: e.message )
+    end
+  end
+
+  def self.import_repo_images(harvest, region, s3, bucket)
+    CSV.parse(harvest.csv_file.download, headers: true) do |row|
+      @repository = Repository.find_by_name(row[2])
+    end
+    case @repository.short_name
+    when "Barbara Bates Center"
+      import_from_bates(harvest, region, s3, bucket)
+    when "Drexel University"
+      import_from_drexel(harvest, region, s3, bucket)
+    when "Haverford College"
+      import_from_haverford(harvest, region, s3, bucket)
+    when "Library Company"
+      import_from_libraryco(harvest, region, s3, bucket)
+    when "Swarthmore - Friends" || "Swarthmore - Peace"
+      import_from_swarthmore(harvest, region, s3, bucket)
+    when "Temple University"
+      import_from_temple(harvest, region, s3, bucket)
+    when "National Archives"
+      import_from_nara(harvest, region, s3, bucket)
+    when "University of Delaware"
+      import_from_udel(harvest, region, s3, bucket)
+    when "The German Society"
+      import_from_german(harvest, region, s3, bucket)
+    when "Bryn Mawr College"
+      import_from_brynmawr(harvest, region, s3, bucket)
+    when "College of Physicians"
+      import_from_physicians(harvest, region, s3, bucket)
+    when "Catholic Historical Research Center"
+      import_from_chrc(harvest, region, s3, bucket)
+    when "Presbyterian Historical Society"
+      import_from_phs(harvest, region, s3, bucket)
+    when "Historical Society of PA"
+      import_from_hsp(harvest, region, s3, bucket)
+    when "Legacy Foundation"
+      import_from_union(harvest, region, s3, bucket)
+    end
+  end
+
+  def self.import_from_bates(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/Bates').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/Bates/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/Bates/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_drexel(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/Drexel').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/Drexel/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/Drexel/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        if dc_identifier.identifier[0..5] == "local:"
+          image_paths.each do |image_path|
+            if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier[7..-1])
+              if image_path[-9..-1] == "thumb.png"
+                record.thumbnail = "/#{image_path}"
+              elsif image_path[-6..-1] == "lg.png"
+                record.file_name = "/#{image_path}"
+              end
+              record.save
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_haverford(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/Haverford').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/Haverford/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/Haverford/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      image_paths.each do |image_path|
+        if !record.oai_identifier.blank? && record.oai_identifier[0..39] == "oai:tricontentdm.brynmawr.edu:HC_DigReq/"
+          if !record.oai_identifier.blank? && image_path.include?(record.oai_identifier[40..-1])
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+        record.dc_identifiers.each do |dc_identifier|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_hsp(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/HSP').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/HSP/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/HSP/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      image_paths.each do |image_path|
+        if !record.oai_identifier.blank? && image_path.include?(record.oai_identifier)
+          if image_path[-9..-1] == "thumb.png"
+            record.thumbnail = "/#{image_path}"
+          elsif image_path[-6..-1] == "lg.png"
+            record.file_name = "/#{image_path}"
+          end
+          record.save
+        end
+      end
+    end
+  end
+
+  def self.import_from_libraryco(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/LibraryCompany').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/LibraryCompany/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/LibraryCompany/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      image_paths.each do |image_path|
+        if !record.oai_identifier.blank? &&  image_path.include?(record.oai_identifier)
+          if image_path[-9..-1] == "thumb.png"
+            record.thumbnail = "/#{image_path}"
+          elsif image_path[-6..-1] == "lg.png"
+            record.file_name = "/#{image_path}"
+          end
+          record.save
+        end
+      end
+    end
+  end
+
+  def self.import_from_swarthmore(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/Swarthmore').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/Swarthmore/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/Swarthmore/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if image_path.include?(dc_identifier.identifier) && !dc_identifier.identifier.blank?
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_temple(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/Temple').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/Temple/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/Temple/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if image_path.include?(dc_identifier.identifier) && !dc_identifier.identifier.blank?
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+
+  def self.import_from_nara(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/NARA').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/NARA/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/NARA/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && (image_path[/NARA\/([^"]+)_lg.png/, 1] == dc_identifier.identifier || image_path[/NARA\/([^"]+)_thumb.png/, 1] == dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_udel(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/UDel').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/UDel/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/UDel/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+
+  def self.import_from_german(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/GermanSociety').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/GermanSociety/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/GermanSociety/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if image_path.include?(dc_identifier.identifier) && !dc_identifier.identifier.blank?
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_brynmawr(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/BrynMawr').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/BrynMawr/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/BrynMawr/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_chrc(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/CHRC').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/CHRC/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/CHRC/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_physicians(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/CollegeOfPhysicians').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/CollegeOfPhysicians/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/CollegeOfPhysicians/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_phs(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/PHS').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/PHS/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/PHS/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+  def self.import_from_union(harvest, region, s3, bucket)
+    all_repo_paths = bucket.objects(prefix: 'images/UnionLeague').collect(&:key)
+    archive_paths = bucket.objects(prefix: 'images/UnionLeague/Archive').collect(&:key)
+    failed_paths = bucket.objects(prefix: 'images/UnionLeague/Failed\ Inbox').collect(&:key)
+    image_paths = all_repo_paths - archive_paths - failed_paths
+    harvest.records.each do |record|
+      record.dc_identifiers.each do |dc_identifier|
+        image_paths.each do |image_path|
+          if !dc_identifier.identifier.blank? && image_path.include?(dc_identifier.identifier)
+            if image_path[-9..-1] == "thumb.png"
+              record.thumbnail = "/#{image_path}"
+            elsif image_path[-6..-1] == "lg.png"
+              record.file_name = "/#{image_path}"
+            end
+            record.save
+          end
+        end
+      end
+    end
+  end
+
+
+  def self.clean_up_collection_imgs
+    collection_records_with_images = Record.all.select { |r| r.is_collection? && (!r.file_name.nil? || !r.file_name.nil?) }
+    collection_records_with_images.each do |r|
+      r.update!(file_name: nil)
+      r.update!(thumbnail: nil)
     end
   end
 end
