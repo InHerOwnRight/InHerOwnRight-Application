71a382ebd9722e95b6b0a9c830e5286389be71d5
JP Lynch <JPLynch35@gmail.com>
2020-12-03 15:49:57 -0500

    [#176001478] Fix oai path render bug

--- app/controllers/pacscl_oai_controller.rb
+++ app/controllers/pacscl_oai_controller.rb
@@ -4,6 +4,6 @@ class PacsclOaiController < ApplicationController
     options = params.delete_if { |k,v| %w{controller action}.include?(k) }
     provider = PacsclOaiProvider.new
     response = provider.process_request(options)
-    render text: response, content_type: "text/xml", layout: false
+    render plain: response, content_type: "text/xml", layout: false
   end
 end
