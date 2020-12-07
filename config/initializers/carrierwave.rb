868cedee4fd14109e3e76ae2ca0ddde8068f90d7
JP Lynch <JPLynch35@gmail.com>
2020-12-07 13:39:26 -0500

    Update carrierwave for S3 public uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,8 @@ else
   CarrierWave.configure do |config|
     config.storage = :aws
     config.aws_bucket = ENV['AWS_BUCKET']
-    config.aws_acl = 'bucket-owner-full-control'
+    config.aws_acl = 'public-read'
+    config.fog_public = true
 
     config.aws_credentials = {
       access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
