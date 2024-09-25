032f34b5d26aa0eab05153e0d3642531e0c6eeb9
Chris Clement <christoph531@gmail.com>
2024-09-24 20:28:54 -0400

    Set User-Agent on openstreetmap API requests

--- app/models/concerns/geocoder_service.rb
+++ app/models/concerns/geocoder_service.rb
@@ -1,14 +1,17 @@
 class GeocoderService
   def self.call_open_street_maps(placename)
+    # Required by OSM: https://operations.osmfoundation.org/policies/nominatim/
+    headers = { "User-Agent": "In Her Own Right"}
+
     sanitized_placename = SpacialSanitizer.execute(placename)
     begin
       request = "https://nominatim.openstreetmap.org/search?format=json&q=#{sanitized_placename}"
-      response = HTTParty.get(request)
+      response = HTTParty.get(request, headers: headers)
     rescue URI::InvalidURIError
       escaped_request = "https://nominatim.openstreetmap.org/search?format=json&q=#{URI.escape(sanitized_placename)}"
-      response = HTTParty.get(escaped_request)
+      response = HTTParty.get(escaped_request, headers: headers)
     end
-    if response.any?
+    if !response.empty?
       OsmApiCall.create(placename: placename, sanitized_placename: sanitized_placename, request: request, response: response)
     else
       call_open_street_maps_without_string_shifting(placename)
@@ -16,13 +19,16 @@ class GeocoderService
   end
 
   def self.call_open_street_maps_without_string_shifting(placename)
+    # Required by OSM: https://operations.osmfoundation.org/policies/nominatim/
+    headers = { "User-Agent": "In Her Own Right"}
+
     sanitized_placename = SpacialSanitizer.execute_without_string_shifting(placename)
     begin
       request = "https://nominatim.openstreetmap.org/search?format=json&q=#{sanitized_placename}"
-      response = HTTParty.get(request)
+      response = HTTParty.get(request, headers: headers)
     rescue URI::InvalidURIError
       escaped_request = "https://nominatim.openstreetmap.org/search?format=json&q=#{URI.escape(sanitized_placename)}"
-      response = HTTParty.get(escaped_request)
+      response = HTTParty.get(escaped_request, headers: headers)
     end
     OsmApiCall.create(placename: placename, sanitized_placename: sanitized_placename, request: request, response: response)
   end
