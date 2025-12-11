8cdd51c0a6a8e2f5b1a609dc3a0438b539247850
Chris Clement <christoph531@gmail.com>
2025-12-11 16:09:32 -0500

    Update entrypoint to configure rails-tls nginx config

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -3,5 +3,8 @@
 SITE_DOMAINS=${SITE_DOMAINS:-inherownright.org}
 
 sed -i "s/SITE_DOMAINS/${SITE_DOMAINS}/g" /etc/nginx/sites-available/rails.conf
+sed -i "s/SITE_DOMAINS/${SITE_DOMAINS}/g" /etc/nginx/sites-available/rails-tls.conf
+
+#certbot -n --nginx --register-unsafely-without-email --agree-tos --domains ${SITE_DOMAINS}
 
 exec "$@"
